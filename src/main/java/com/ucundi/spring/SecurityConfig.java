9d14c1e58f59fbbf67d395a35f3d3f484f229de8
juan diego pachon poveda <juandi240298@hotmail.com>
2023-09-08 13:53:33 -0500

    subida de repositorio

---
+++ src/main/java/com/ucundi/spring/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.ucundi.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        //Control access privileges
+                        .requestMatchers("/camas").authenticated()
+                        .requestMatchers("/comida").authenticated()
+                        .requestMatchers("/ropa").permitAll()
+                        .requestMatchers("/servicios","/juguetes").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .formLogin(withDefaults());
+        // @formatter:on
+        return http.build();
+    }
+
+    // @formatter:off
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        PasswordEncoder passwordEncoder = passwordEncoder();
+
+        UserDetails user = User.builder()
+                .username("administrador")
+                .password(passwordEncoder.encode("57r6br)TsD?}ñ"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+      }  
+
+}
